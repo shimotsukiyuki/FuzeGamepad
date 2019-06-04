95edbe5406476728d7d2892e2ec618cfdb1072ab
mumumusuc <mumumusuc@gmail.com>
2019-06-04 15:06:41 +0800

    switch to HidLibraryCompatible & add bt support

--- src/Fuze/Program.cs
+++ src/Fuze/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HidLibraryCompatible;
 using HidLibrary;
 using ScpDriverInterface;
 using System.Threading;
@@ -10,211 +11,93 @@ using System.Runtime.InteropServices;
 
 namespace Fuze
 {
-    class Program
-    {
-        private static ScpBus global_scpBus;
-        static bool ConsoleEventCallback(int eventType)
-        {
-            if (eventType == 2)
-            {
-                global_scpBus.UnplugAll();
-            }
-            return false;
-        }
-        static ConsoleEventDelegate handler;   // Keeps it from getting garbage collected
-                                               // Pinvoke
-        private delegate bool ConsoleEventDelegate(int eventType);
-        [DllImport("kernel32.dll", SetLastError = true)]
-        private static extern bool SetConsoleCtrlHandler(ConsoleEventDelegate callback, bool add);
+	class Program
+	{
+		static readonly int[] VIDs = { 0x0079, 0x12D1 };
+		static readonly int[] PIDs = { 0x181c, 0xA560 };
+		const bool isExclusiveMode = false;
+		
+		static ScpBus global_scpBus;
+		static bool ConsoleEventCallback(int eventType)
+		{
+			if (eventType == 2) {
+				global_scpBus.UnplugAll();
+			}
+			return false;
+		}
+		static ConsoleEventDelegate handler;
+		// Keeps it from getting garbage collected
+		// Pinvoke
+		private delegate bool ConsoleEventDelegate(int eventType);
+		[DllImport("kernel32.dll", SetLastError = true)]
+		private static extern bool SetConsoleCtrlHandler(ConsoleEventDelegate callback, bool add);
 
 
 
-        public static string ByteArrayToHexString(byte[] bytes)
-        {
-            return string.Join(string.Empty, Array.ConvertAll(bytes, b => b.ToString("X2")));
-        }
+		public static string ByteArrayToHexString(byte[] bytes)
+		{
+			return string.Join(string.Empty, Array.ConvertAll(bytes, b => b.ToString("X2")));
+		}
 
 
 
-        static void Main(string[] args)
-        {
-            while (true)
-            {
-                try
-                {
-                    Init();
-                }
-                catch (Exception e)
-                {
-                    Log.WriteLine(e.Message,Log.LogType.exception);
-                }
+		static void Main(string[] args)
+		{
+			while (true) {
+				try {
+					Init();
+				} catch (Exception e) {
+					Log.WriteLine(e.Message, Log.LogType.exception);
+				}
 
-                Log.WriteLine("An exception occurred. Press ENTER to retry, or Alt+F4 to exit.");
-                Console.ReadLine();
+				Log.WriteLine("An exception occurred. Press ENTER to retry, or Alt+F4 to exit.");
+				Console.ReadLine();
 
-            }
-        }
+			}
+		}
 
-        private static void Init()
-        {
-            ScpBus scpBus = new ScpBus();
-            scpBus.UnplugAll();
-            global_scpBus = scpBus;
+		private static void Init()
+		{
+			ScpBus scpBus = new ScpBus();
+			scpBus.UnplugAll();
+			global_scpBus = scpBus;
 
-            handler = new ConsoleEventDelegate(ConsoleEventCallback);
-            SetConsoleCtrlHandler(handler, true);
+			handler = new ConsoleEventDelegate(ConsoleEventCallback);
+			SetConsoleCtrlHandler(handler, true);
 
-            var compatibleDevices = HidDevices.Enumerate(0x79, 0x181c).ToList();
-            Thread.Sleep(400);
+			var compatibleDevices = HidDevices.Enumerate().ToList();
+			Thread.Sleep(400);
 
-            FuzeGamepad[] gamepads = new FuzeGamepad[4];
-            int index = 1;
-            //compatibleDevices.RemoveRange(1, compatibleDevices.Count - 1);
-            foreach (var deviceInstance in compatibleDevices)
-            {
-                //Console.WriteLine(deviceInstance);
-                HidDevice Device = deviceInstance;
-                try
-                {
-                    Device.OpenDevice(DeviceMode.Overlapped, DeviceMode.Overlapped, ShareMode.Exclusive);
-                }
-                catch
-                {
-                    Log.WriteLine("Could not open gamepad in exclusive mode. Try re-enable device.",Log.LogType.warning);
-                    var instanceId = devicePathToInstanceId(deviceInstance.DevicePath);
-                    if (TryReEnableDevice(instanceId))
-                    {
-                        try
-                        {
-                            Device.OpenDevice(DeviceMode.Overlapped, DeviceMode.Overlapped, ShareMode.Exclusive);
-                            Log.WriteLine("Opened in exclusive mode.");
-                        }
-                        catch
-                        {
-                            Device.OpenDevice(DeviceMode.Overlapped, DeviceMode.Overlapped, ShareMode.ShareRead | ShareMode.ShareWrite);
-                            Log.WriteLine("Opened in shared mode.");
-                        }
-                    }
-                    else
-                    {
-                        Device.OpenDevice(DeviceMode.Overlapped, DeviceMode.Overlapped, ShareMode.ShareRead | ShareMode.ShareWrite);
-                        Log.WriteLine("Opened in shared mode.");
-                    }
-                }
+			FuzeGamepad[] gamepads = new FuzeGamepad[4];
+			int index = 1;
+			
+			foreach (var deviceInstance in compatibleDevices) {
+				if (!((IList<int>)VIDs).Contains(deviceInstance.Attributes.VendorId)
+				    || !((IList<int>)PIDs).Contains(deviceInstance.Attributes.ProductId)) {
+					continue;
+				}
+				Console.WriteLine(deviceInstance);
+				HidLibraryCompatible.HidDevice Device = deviceInstance;
+				if (!Device.IsOpen) {
+					try {
+						Device.OpenDevice(isExclusiveMode);
+					} catch (Exception e) {
+						Log.WriteLine(string.Format("Open Hid device error\n{0}", e), Log.LogType.error);
+					}
+				}
+				gamepads[index - 1] = new FuzeGamepad(HidLibrary.HidDevice.wrapper(Device), scpBus, index);
+				if (++index >= 5) {
+					break;
+				}
+			}
 
-                //byte[] Vibration = { 0x20, 0x00, 0x00 };
-                //if (Device.WriteFeatureData(Vibration) == false)
-                //{
-                //    Console.WriteLine("Could not write to gamepad (is it closed?), skipping");
-                //    Device.CloseDevice();
-                //    continue;
-                //}
+			Log.WriteLine(string.Format("{0} controllers connected", index - 1));
 
-                byte[] serialNumber;
-                byte[] product;
-                Device.ReadSerialNumber(out serialNumber);
-                Device.ReadProduct(out product);
+			while (true) {
+				Thread.Sleep(1000);
+			}
+		}
+		
 
-
-                gamepads[index - 1] = new FuzeGamepad(Device, scpBus, index);
-                ++index;
-
-                if (index >= 5)
-                {
-                    break;
-                }
-            }
-
-            Log.WriteLine(string.Format("{0} controllers connected", index - 1));
-
-            while (true)
-            {
-                Thread.Sleep(1000);
-            }
-        }
-
-        private static bool TryReEnableDevice(string deviceInstanceId)
-        {
-            try
-            {
-                bool success;
-                Guid hidGuid = new Guid();
-                HidLibrary.NativeMethods.HidD_GetHidGuid(ref hidGuid);
-                IntPtr deviceInfoSet = HidLibrary.NativeMethods.SetupDiGetClassDevs(ref hidGuid, deviceInstanceId, 0, HidLibrary.NativeMethods.DIGCF_PRESENT | HidLibrary.NativeMethods.DIGCF_DEVICEINTERFACE);
-                HidLibrary.NativeMethods.SP_DEVINFO_DATA deviceInfoData = new HidLibrary.NativeMethods.SP_DEVINFO_DATA();
-                deviceInfoData.cbSize = Marshal.SizeOf(deviceInfoData);
-                success = HidLibrary.NativeMethods.SetupDiEnumDeviceInfo(deviceInfoSet, 0, ref deviceInfoData);
-                if (!success)
-                {
-                    Log.WriteLine("Error getting device info data, error code = " + Marshal.GetLastWin32Error(),
-                        Log.LogType.error);
-                }
-                success = HidLibrary.NativeMethods.SetupDiEnumDeviceInfo(deviceInfoSet, 1, ref deviceInfoData); // Checks that we have a unique device
-                if (success)
-                {
-                    Log.WriteLine("Can't find unique device",Log.LogType.error);
-                }
-
-                HidLibrary.NativeMethods.SP_PROPCHANGE_PARAMS propChangeParams = new HidLibrary.NativeMethods.SP_PROPCHANGE_PARAMS();
-                propChangeParams.classInstallHeader.cbSize = Marshal.SizeOf(propChangeParams.classInstallHeader);
-                propChangeParams.classInstallHeader.installFunction = HidLibrary.NativeMethods.DIF_PROPERTYCHANGE;
-                propChangeParams.stateChange = HidLibrary.NativeMethods.DICS_DISABLE;
-                propChangeParams.scope = HidLibrary.NativeMethods.DICS_FLAG_GLOBAL;
-                propChangeParams.hwProfile = 0;
-                success = HidLibrary.NativeMethods.SetupDiSetClassInstallParams(deviceInfoSet, ref deviceInfoData, ref propChangeParams, Marshal.SizeOf(propChangeParams));
-                if (!success)
-                {
-                    Log.WriteLine("Error setting class install params, error code = " + Marshal.GetLastWin32Error()
-                        , Log.LogType.error);
-                    return false;
-                }
-                success = HidLibrary.NativeMethods.SetupDiCallClassInstaller(HidLibrary.NativeMethods.DIF_PROPERTYCHANGE, deviceInfoSet, ref deviceInfoData);
-                if (!success)
-                {
-                    Log.WriteLine("Error disabling device, error code = " + Marshal.GetLastWin32Error()
-                        , Log.LogType.error);
-                    return false;
-
-                }
-                propChangeParams.stateChange = HidLibrary.NativeMethods.DICS_ENABLE;
-                success = HidLibrary.NativeMethods.SetupDiSetClassInstallParams(deviceInfoSet, ref deviceInfoData, ref propChangeParams, Marshal.SizeOf(propChangeParams));
-                if (!success)
-                {
-                    Log.WriteLine("Error setting class install params, error code = " + Marshal.GetLastWin32Error()
-                        , Log.LogType.error);
-                    return false;
-                }
-                success = HidLibrary.NativeMethods.SetupDiCallClassInstaller(HidLibrary.NativeMethods.DIF_PROPERTYCHANGE, deviceInfoSet, ref deviceInfoData);
-                if (!success)
-                {
-                    Log.WriteLine("Error enabling device, error code = " + Marshal.GetLastWin32Error()
-                        , Log.LogType.error);
-                    return false;
-                }
-
-                HidLibrary.NativeMethods.SetupDiDestroyDeviceInfoList(deviceInfoSet);
-
-                return true;
-            }
-            catch
-            {
-                Log.WriteLine("Can't reenable device"
-                    , Log.LogType.error);
-                return false;
-            }
-        }
-
-        private static string devicePathToInstanceId(string devicePath)
-        {
-            string deviceInstanceId = devicePath;
-            deviceInstanceId = deviceInstanceId.Remove(0, deviceInstanceId.LastIndexOf('\\') + 1);
-            deviceInstanceId = deviceInstanceId.Remove(deviceInstanceId.LastIndexOf('{'));
-            deviceInstanceId = deviceInstanceId.Replace('#', '\\');
-            if (deviceInstanceId.EndsWith("\\"))
-            {
-                deviceInstanceId = deviceInstanceId.Remove(deviceInstanceId.Length - 1);
-            }
-            return deviceInstanceId;
-        }
-    }
+	}
 }
---
+++ src/HidLibraryCompatible/HidDeviceCompatible.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using HidLibraryCompatible;
+
+namespace HidLibrary
+{
+	
+	public class HidDeviceData
+	{
+		public enum ReadStatus
+		{
+			Success = 0,
+			WaitTimedOut = 1,
+			WaitFail = 2,
+			NoDataRead = 3,
+			ReadError = 4,
+			NotConnected = 5
+		}
+
+		public HidDeviceData(ReadStatus status)
+		{
+			Data = new byte[] { };
+			Status = status;
+		}
+
+		public HidDeviceData(byte[] data, ReadStatus status)
+		{
+			Data = data;
+			Status = status;
+		}
+
+		public byte[] Data { get; private set; }
+		public ReadStatus Status { get; private set; }
+	}
+	
+	public class HidDevice
+	{
+		readonly HidLibraryCompatible.HidDevice proxy;
+
+		internal HidDevice(HidLibraryCompatible.HidDevice dev)
+		{
+			proxy = dev;
+		}
+		
+		public static HidDevice wrapper(HidLibraryCompatible.HidDevice dev)
+		{
+			return new HidDevice(dev);
+		}
+
+		public IntPtr Handle { get; private set; }
+		public bool IsOpen { get; private set; }
+		public bool IsConnected { get { return proxy.IsConnected; } }
+		public string Description { get { return proxy.Description; } }
+		public HidDeviceCapabilities Capabilities { get { return proxy.Capabilities; } }
+		public HidDeviceAttributes Attributes { get { return proxy.Attributes; } }
+		public string DevicePath { get { return proxy.DevicePath; } }
+
+		public override string ToString()
+		{
+			return proxy.ToString();
+		}
+
+		public void OpenDevice()
+		{
+			proxy.OpenDevice(false);
+		}
+
+		public void CloseDevice()
+		{
+			proxy.CloseDevice();
+		}
+
+		public HidDeviceData Read()
+		{
+			return Read(0);
+		}
+
+		public HidDeviceData Read(int timeout)
+		{
+			var buffer = new byte[]{ };
+			switch (proxy.ReadWithFileStream(buffer, timeout)) {
+				case HidLibraryCompatible.HidDevice.ReadStatus.Success:
+					return new HidDeviceData(buffer, HidDeviceData.ReadStatus.Success);
+				case HidLibraryCompatible.HidDevice.ReadStatus.ReadError:
+					return new HidDeviceData(HidDeviceData.ReadStatus.ReadError);
+				case HidLibraryCompatible.HidDevice.ReadStatus.NoDataRead:
+					return new HidDeviceData(HidDeviceData.ReadStatus.NoDataRead);
+			}
+			return new HidDeviceData(HidDeviceData.ReadStatus.WaitTimedOut);	
+		}
+
+		public HidReport ReadReport()
+		{
+			return ReadReport(0);
+		}
+
+		public HidReport ReadReport(int timeout)
+		{
+			return new HidReport(Capabilities.InputReportByteLength, Read(timeout));
+		}
+
+		/*
+		public bool ReadFeatureData(out byte[] data, byte reportId = 0)
+		{
+			return false;
+		}
+
+		public bool ReadProduct(out byte[] data)
+		{
+			return false;
+			
+		}
+
+		public bool ReadManufacturer(out byte[] data)
+		{
+			return false;
+			
+		}
+
+		public bool ReadSerialNumber(out byte[] data)
+		{
+			return false;
+			
+		}
+*/
+		public bool Write(byte[] data)
+		{
+			return Write(data, 0);
+		}
+
+		public bool Write(byte[] data, int timeout)
+		{
+			// TODO
+			return false;
+		}
+
+		public bool WriteReport(HidReport report)
+		{
+			return WriteReport(report, 0);
+		}
+
+		public bool WriteReport(HidReport report, int timeout)
+		{
+			return Write(report.GetBytes(), timeout);
+		}
+
+
+		public HidReport CreateReport()
+		{
+			return new HidReport(Capabilities.OutputReportByteLength);
+		}
+
+		public bool WriteFeatureData(byte[] data)
+		{
+			return false;
+			
+		}
+
+		public void Dispose()
+		{
+			proxy.Dispose();
+		}
+	}
+	
+	public class HidReport
+	{
+		private byte _reportId;
+		private byte[] _data = new byte[] { };
+
+		private readonly HidDeviceData.ReadStatus _status;
+
+		public HidReport(int reportSize)
+		{
+			Array.Resize(ref _data, reportSize - 1);
+		}
+
+		public HidReport(int reportSize, HidDeviceData deviceData)
+		{
+			_status = deviceData.Status;
+
+			Array.Resize(ref _data, reportSize - 1);
+
+			if ((deviceData.Data != null)) {
+
+				if (deviceData.Data.Length > 0) {
+					_reportId = deviceData.Data[0];
+					Exists = true;
+
+					if (deviceData.Data.Length > 1) {
+						var dataLength = reportSize - 1;
+						if (deviceData.Data.Length < reportSize - 1)
+							dataLength = deviceData.Data.Length;
+						Array.Copy(deviceData.Data, 1, _data, 0, dataLength);
+					}
+				} else
+					Exists = false;
+			} else
+				Exists = false;
+		}
+
+		public bool Exists { get; private set; }
+		public HidDeviceData.ReadStatus ReadStatus { get { return _status; } }
+
+		public byte ReportId {
+			get { return _reportId; }
+			set {
+				_reportId = value;
+				Exists = true;
+			}
+		}
+
+		public byte[] Data {
+			get { return _data; }
+			set {
+				_data = value;
+				Exists = true;
+			}
+		}
+
+		public byte[] GetBytes()
+		{
+			byte[] data = null;
+			Array.Resize(ref data, _data.Length + 1);
+			data[0] = _reportId;
+			Array.Copy(_data, 0, data, 1, _data.Length);
+			return data;
+		}
+	}
+}
